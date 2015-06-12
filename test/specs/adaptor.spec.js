cf4f0e2b3a68fd298fe71042a56569b8d765bb0c
deadprogram <ron@hybridgroup.com>
2015-06-11 20:22:56 -0700

    Initial commit

---
+++ test/specs/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Muse = source("adaptor");
+
+describe("Cylon.Adaptors.Muse", function() {
+  var adaptor = new Muse();
+
+  it("needs tests");
+});
---
+++ test/specs/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Muse = source("driver");
+
+describe("Cylon.Drivers.Muse", function() {
+  var driver = new Muse({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
